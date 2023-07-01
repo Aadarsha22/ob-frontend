bc68ed9285a0b0151afd050ded3dce33538d0b17
Aadarsha22 <130428086+Aadarsha22@users.noreply.github.com>
2023-07-01 10:25:54 +0545

    final frontend

--- src/protected/UserRoute.js
+++ src/protected/UserRoute.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Navigate } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 
 const UserRoute = () => {
     const user = JSON.parse(localStorage.getItem("user"));
